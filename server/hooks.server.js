01eea0cdaaee8579e29df722fe003f283fba8fdf
Brewhouse Digital <66521220+brewhousedigital@users.noreply.github.com>
2023-03-19 10:08:11 -0500

    Create hooks.server.js

---
+++ server/hooks.server.js
@@ -0,0 +1,33 @@
+import {redirect} from '@sveltejs/kit';
+
+
+// If you want to restrict access to a specific folder or page, you can watch for all request events via the Hooks file
+// This means that every page needing to be restricted will also require a +page.server.js file or a +layout.server.js file
+// Static pages (without the server files) are not passed through this Hooks event
+export const handle = async({ event, resolve }) => {
+  const currentPath = event.url.pathname;
+  const cookies = event.cookies;
+  
+  // Validate the user's current auth token
+  const isTokenValid = validateYourTokenHere(cookies)
+  
+  // Restrict all routes under /protected
+  if(currentPath.includes("/protected")) {
+    if(!isTokenValid) {
+      throw redirect(303, "/");
+    }
+  }
+
+  // Resolve as normal
+  const response = await resolve(event);
+
+  return response;
+}
+
+
+// Example of how to read the cookies
+// Svelte has built-in methods for accessing cookies, but ideally this would use your authentication library's SDK instead
+const validateYourTokenHere = (cookies) => {
+  const currentToken = cookies.get("my-auth-token");
+  return currentToken === "123;
+}
